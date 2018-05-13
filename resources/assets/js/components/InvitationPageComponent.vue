22522b76d064177813fce29a0d6ca9fa96e6e4cf
Andrei Gavrila <Andrei.Gavrila@catalysts.cc>
2018-05-13 13:41:09 +0300

    Design invitation that will be used by admins to invite doctors and staff.

---
+++ resources/assets/js/components/InvitationPageComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col-xs-12 col-sm-8 offset-sm-2">
+        <div class="card">
+            <div class="card-body">
+                <form @submit.prevent="invite()">
+                    <div class="form-group">
+                        <label for="email" class="sr-only">Email</label>
+                        <input id="email" class="form-control col" type="email" v-model="email" placeholder="Email"
+                               required>
+                    </div>
+                    <div class="form-group">
+                        <label for="user_type" class="sr-only">User type</label>
+                        <select id="user_type" class="custom-select" v-model="userType" required>
+                            <option value="" selected>Select user type</option>
+                            <option value="assistant">Assistant</option>
+                            <option value="doctor">Doctor</option>
+                        </select>
+                    </div>
+                    <input type="submit" class="btn btn-primary btn-block" value="Invite">
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                email: "",
+                userType: "",
+            }
+        },
+        methods: {
+            invite() {
+                console.log(event);
+            }
+        }
+    }
+</script>
