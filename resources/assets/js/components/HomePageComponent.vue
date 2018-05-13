606517b124d672b6bb2d195eb781173cf3780db8
LorenaSabou <lorenasabou@yahoo.com>
2018-05-13 19:54:22 +0300

    Homepage made. The icons are not properly alligned with the text tho

--- resources/assets/js/components/HomePageComponent.vue
+++ resources/assets/js/components/HomePageComponent.vue
@@ -1,6 +1,38 @@
 <template>
     <div>
-        homepage, fuck yeah
+        <h3>Donation may be done by anoyone who satifies the following criterias:</h3>
+
+        <i class="fas fa-check-circle fa-2x green"></i>   Any Romanian citizen or EU citizen living in Romania, in good health<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	Age between 18-60<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	Weight above 50Kg<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	Stable pulse, 60-100 beets/minute<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	Systolic blood pressure between 100 and 180 mmHg<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	No surgeries in the last 6 months<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	Women must not be: pregnant, post birth, or menstruating<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	No alcohol or fat consumption in the last 48 hours<br>
+		<i class="fas fa-check-circle fa-2x green"></i>	Must not be in treatment for: hypertension, heart disease, kidney disease, mental illness, blood disease, or endocrine affections<br>
+
+        <h3>The donor must have or have had the following diseases:</h3>
+        <i class="fas fa-times-circle fa-2x red"></i> Hepatitis (of any type)<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	TB<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Pox<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Malaria<br>
+        <i class="fas fa-times-circle fa-2x red"></i> Epilepsy and other neurological diseases<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Mental illness<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Brucellosis<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Ulcer<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Diabetes<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Heart diseases<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Skin diseases: Psoriasis, Vitiligo<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Myopia over (-) 6 diopters<br>
+        <i class="fas fa-times-circle fa-2x red"></i>	Cancer<br>
+
+        <h3>Before donation: </h3>
+        <i class="fas fa-info-circle fa-2x blue"></i>	The morning before blood donation you may drink a cup of tea or coffee, and eat a light breakfast of fruits and vegetables<br>
+        <i class="fas fa-info-circle fa-2x blue"></i>	Do not smoke and hour before and after blood donation<br>
+        <i class="fas fa-info-circle fa-2x blue"></i>	Arrive well slept, and not after a night of working or staying up<br>
+
+
     </div>
 </template>
 
@@ -10,5 +42,25 @@
 </script>
 
 <style scoped>
+ .red{
+     color :#dc3545;
+     margin-top: 10px;
+     margin-right: 10px;
+ }
+ .green{
+     color: limegreen;
+     margin-top: 10px;
+     margin-right: 10px;
+ }
+
+ .blue {
+     color: cornflowerblue;
+     margin-top: 10px;
+     margin-right: 10px;
+ }
+ h3 {
+     margin-top:50px;
+     margin-bottom:20px;
+ }
 
 </style>
