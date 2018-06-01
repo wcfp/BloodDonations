5c3a690c3ac3b459446054a06497f76a63b3a298
sergiu.97 <thisis.sergiu@gmail.com>
2018-06-01 23:55:47 +0300

    pwp erorile

--- resources/assets/js/components/RegisterDonationComponent.vue
+++ resources/assets/js/components/RegisterDonationComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h2>Please choose the date for your next appointment: </h2>
+        <p v-if="errors.length>0" class="text-danger">{{errors[0]}}</p>
         <button @click="createAppointment()">Create</button>
     </div>
 
@@ -8,11 +9,19 @@
 
 <script>
     export default {
-
+        data() {
+            return {
+                errors: {}
+            }
+        },
         methods: {
             createAppointment() {
                 this.$store.dispatch('createAppointment', {date: '2018-07-08 09:20:00'})
-                    .then(response => this.$router.push('/history'));
+                    .then(response => this.$router.push('/history'))
+                    .catch(error => {
+                        //console.log(error);
+                        this.errors = error.data.message;
+                    });
             }
         }
     }
